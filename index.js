21b8729fd80eadebb5d652000ad13952f47908b7
Your Name <you@example.com>
2022-10-21 00:08:45 +0600

    2nd commit

--- index.js
+++ index.js
@@ -5,10 +5,32 @@ const port = 5000
 
 app.use(cors())
 
-const categories = require('./data/categories.json')
+const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.get('/news-categories', (req, res) => {
   res.send(categories)
+})
+
+app.get('/news/:id', (req,res) => {
+  const id = req.params.id;
+  const selectedNews = news.find(n => n._id === id);
+  res.send(selectedNews)
+  // console.log(req.params.id);
+})
+
+app.get('/category/:id' , (req,res) => {
+    const id = req.params.id;
+    if (id === '08') {
+      res.send(news)
+    } else {
+      const categoryNews = news.filter(n => n.category_id === id) ;
+      res.send(categoryNews)
+    }
+})
+
+app.get('/news', (req,res) => {
+  res.send(news);
 })
 
 app.listen(port, () => {
